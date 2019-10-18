deb8d2a584a2cbea40b252619f16b735ffb1069c
Compl Yue <compl.yue@icloud.com>
2019-10-18 22:43:44 +0800

    refine

--- demo/src/PoC.hs
+++ demo/src/PoC.hs
@@ -1,19 +1,22 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ExtendedDefaultRules #-}
 
 module PoC
     ( demoProcedure
     )
 where
 
-import           RIO
 
+import           RIO
 import           UIO
+
+default (Text, Int)
 
 
 demoProcedure :: UIO ()
 demoProcedure = do
-    uiLog "This log msg should appear on front UI."
+    uiClearLog -- clear frontend log box
+    print "This log msg should appear on front UI."
     logInfo "This log msg should appear on backend."
-
 

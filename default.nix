22f3c9e5eb0940a9885bcbd5791b82f638d95158
ComplYue <compl.yue@icloud.com>
2019-11-19 22:36:50 +0800

    upd

--- default.nix
+++ default.nix
@@ -2,8 +2,8 @@
 import (
 # to use a version of Hadui from github
   builtins.fetchTarball {
-    url = "https://github.com/complyue/hadui/archive/0.1.0.rc2.tar.gz";
-    sha256 = "1d5jvgm584ciz4d89kvmlcg02sbc0567jqraqn3vqgrjipx006s9";
+    url = "https://github.com/complyue/hadui/archive/0.1.0.rc3.tar.gz";
+    sha256 = "1pa13r33smzni1r263sbz3q2iay4qyavmcxpaslafzc1rh548hbj";
   }
 
   # to use the version of Hadui checked out locally
